f669c5eb943036febb3ed7547f671b96f0d9d74e
Jansen <jansenv44@gmail.com>
2020-04-26 17:44:08 -0500

    now accepting user input

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+	
+		Scanner userInputScanner = new Scanner(System.in);
+
+		System.out.print("What's your name?");
+
+		String userInputName = userInputScanner.nextLine();
+
+		System.out.println("Hello " + userInputName + "!");
+
 	}
 }
-
